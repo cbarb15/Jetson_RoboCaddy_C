a352bd9f7d1587eef3e466e570d1d0bd8a0e44ab
cbarb2747 <cbarb2747@gmail.com>
2026-04-22 15:35:56 -0700

    Configured terminos for uart communication

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
 
+#include "../../../../../usr/include/errno.h"
+
 using namespace std;
 
 /* Global variable to interrupt the loop later on*/
@@ -24,16 +26,45 @@ void connect(SimpleBLE::Peripheral* peripheral);
 void setupGPIO();
 
 int main() {
-    setupGPIO();
+    // setupGPIO();
 
     int serial_port = open("/dev/ttyACM0", O_RDWR);
     if (serial_port < 0) {
         cout << "Error opening serial port " << serial_port << endl;
     }
+    cout << "Serial port opened" << endl;
 
     if (tcgetattr(serial_port, &tty) != 0) {
         cout << "Error " << errno << " from tcgetattr() " << endl;
     }
+    cout << "Got terminos struct to configure uart settings" << endl;
+
+    tty.c_cflag &= ~PARENB;        // No parity
+    tty.c_cflag &= ~CSTOPB;        // 1 Stop bit
+    tty.c_cflag &= ~CSIZE;         // Clear size
+    tty.c_cflag |= CS8;            // 8 bits per byte
+    tty.c_cflag &= ~CRTSCTS;       // Disable hardware control flow
+    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+    tty.c_lflag &= ~ICANON;        // Disable canonical mode
+    tty.c_lflag &= ~ECHO;          // Disable echo
+    tty.c_lflag &= ~ECHOE;         // Disable erasure
+    tty.c_lflag &= ~ECHONL;        // Disable new-line echo
+    tty.c_lflag &= ~ISIG;          // Disable interpretation of INTR, QUIT and SUSP
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+    tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VMIN] = 0;
+    cfsetispeed(&tty, B115200);
+    cfsetospeed(&tty, B115200); // BAUD Rate 115200
+
+    // Saving terminos with config
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+        cout << "Error " << strerror(errno) << " from tcsetattr" << endl;
+    }
+    cout << "Terminos saved with config" << endl;
+
     // peripheral.disconnect();
 }
 
