8f545912de4198914eb4f859a14397e34313b4b4
cbarb2747 <cbarb2747@gmail.com>
2026-04-25 11:57:26 -0700

    Added threads and running gpio and serial thread

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 #include <errno.h> // Error integer and strerror() function
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
+#include <thread>
+#include <chrono>
 
 #include "../../../../../usr/include/errno.h"
 
@@ -20,59 +22,27 @@ using namespace std;
 static volatile int interrupt = 1;
 unsigned long timestamp;
 struct termios tty;
+int serial_port;
 
 void searchAndConnect();
 void connect(SimpleBLE::Peripheral* peripheral);
 void setupGPIO();
+void setupSerialComm();
 
 int main() {
-    // setupGPIO();
-
-    int serial_port = open("/dev/ttyACM2", O_RDWR);
-    if (serial_port < 0) {
-        cout << "Error opening serial port " << serial_port << endl;
-    }
-    cout << "Serial port opened" << endl;
-
-    if (tcgetattr(serial_port, &tty) != 0) {
-        cout << "Error " << errno << " from tcgetattr() " << endl;
-    }
-    cout << "Got terminos struct to configure uart settings" << endl;
-
-    tty.c_cflag &= ~PARENB;        // No parity
-    tty.c_cflag &= ~CSTOPB;        // 1 Stop bit
-    tty.c_cflag &= ~CSIZE;         // Clear size
-    tty.c_cflag |= CS8;            // 8 bits per byte
-    tty.c_cflag &= ~CRTSCTS;       // Disable hardware control flow
-    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-    tty.c_lflag &= ~ICANON;        // Disable canonical mode
-    tty.c_lflag &= ~ECHO;          // Disable echo
-    tty.c_lflag &= ~ECHOE;         // Disable erasure
-    tty.c_lflag &= ~ECHONL;        // Disable new-line echo
-    tty.c_lflag &= ~ISIG;          // Disable interpretation of INTR, QUIT and SUSP
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-    tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-    tty.c_cc[VMIN] = 0;
-    cfsetispeed(&tty, B115200);
-    cfsetospeed(&tty, B115200); // BAUD Rate 115200
-
-    // Saving terminos with config
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-        cout << "Error " << strerror(errno) << " from tcsetattr" << endl;
-    }
-    cout << "Terminos saved with config" << endl;
+    thread gpioThread(setupGPIO);
+    thread serialThread(setupSerialComm);
 
     unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '\r'};
-    while (true) {
-        write(serial_port, msg, sizeof(msg));
-        usleep(5000);
-        cout << "Sending Hello over serial" << endl;
-    }
+    this_thread::sleep_for(chrono::seconds(5));
+    cout << "Sending Hello over serial" << endl;
+    write(serial_port, msg, sizeof(msg));
 
-    // peripheral.disconnect();
+    gpioThread.join();
+    serialThread.join();
+
+    return 0;
+
 }
 
 
@@ -149,6 +119,45 @@ void setupGPIO()
     exit(0);
 }
 
+void setupSerialComm() {
+    serial_port = open("/dev/ttyACM2", O_RDWR);
+    if (serial_port < 0) {
+        cout << "Error opening serial port " << serial_port << endl;
+    }
+    cout << "Serial port opened" << endl;
+
+    if (tcgetattr(serial_port, &tty) != 0) {
+        cout << "Error " << errno << " from tcgetattr() " << endl;
+    }
+    cout << "Got terminos struct to configure uart settings" << endl;
+
+    tty.c_cflag &= ~PARENB;        // No parity
+    tty.c_cflag &= ~CSTOPB;        // 1 Stop bit
+    tty.c_cflag &= ~CSIZE;         // Clear size
+    tty.c_cflag |= CS8;            // 8 bits per byte
+    tty.c_cflag &= ~CRTSCTS;       // Disable hardware control flow
+    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+    tty.c_lflag &= ~ICANON;        // Disable canonical mode
+    tty.c_lflag &= ~ECHO;          // Disable echo
+    tty.c_lflag &= ~ECHOE;         // Disable erasure
+    tty.c_lflag &= ~ECHONL;        // Disable new-line echo
+    tty.c_lflag &= ~ISIG;          // Disable interpretation of INTR, QUIT and SUSP
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+    tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VMIN] = 0;
+    cfsetispeed(&tty, B115200);
+    cfsetospeed(&tty, B115200); // BAUD Rate 115200
+
+    // Saving terminos with config
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+        cout << "Error " << strerror(errno) << " from tcsetattr" << endl;
+    }
+    cout << "Terminos saved with config" << endl;
+}
+
 void searchAndConnect()
 {
     string DEVICE_ADDRESS = "2C:CF:67:46:97:7F";
@@ -183,6 +192,7 @@ void searchAndConnect()
     cout << "Device To Connect To" << peripheral.identifier() << " [" << peripheral.address() << "]" << endl;
 
     // TODO: This wil be conditional. It will call disconnect if the button is pressed and connected
+    // peripheral.disconnect();
     connect(&peripheral);
 }
 
